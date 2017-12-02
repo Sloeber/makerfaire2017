22cedf45e43aa688877d385e5a026d451ada9680
Wim Jongman <wim.jongman@remainsoftware.com>
2017-12-02 19:25:47 +0100

    Initial commit

---
+++ makerfaire2017/sloeber.ino.cpp
@@ -0,0 +1,21 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-12-02 19:19:27
+
+#include "Arduino.h"
+#include "makerfairRome2017Demo.h"
+
+void setup() ;
+void loop() ;
+int getBlinkLedValue(const uint32_t currentMillis, const uint32_t onInterval, 		const uint32_t offInterval) ;
+int getFadeLedValue(const uint32_t currentMillis, const uint32_t onInterval, 		const uint32_t offInterval) ;
+uint32_t numTriggers(const uint32_t currentMillis, 		const uint32_t triggerInterval) ;
+bool shouldIPlot(const uint32_t currentMillis);
+bool shouldIPrintKeepAliveMessage(const uint32_t currentMillis);
+
+
+#include "makerfairRome2017Demo.ino"
+
+#endif
